4747172089d0b2af568a4c1391ea12438e96a42c
Louis Pearson <louispearson@librem.one>
2021-04-18 03:21:21 -0600

    Add levels and speed

--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,7 @@ const Piece = @import("piece.zig").Piece;
 const PieceType = @import("piece.zig").PieceType;
 const util = @import("util.zig");
 const get_score = @import("score.zig").get_score;
+const get_drop_delta = @import("score.zig").get_drop_delta;
 
 pub fn main() void {
     seizer.run(.{
@@ -44,20 +45,25 @@ const Context = struct {
     last_time: f64,
     bag: [7]PieceType,
     grab: usize,
-    cleared_rows: [4]?usize,
+    cleared_rows: usize,
     score: usize,
+    level: usize,
+    level_at: usize,
 
     // Rendering variables
     flat: FlatRenderer,
     font: FontRenderer,
     tileset_tex: Texture,
     score_text: []u8,
+    level_text: []u8,
+    lines_text: []u8,
 };
 var ctx: Context = undefined;
 
 pub fn onInit() !void {
     var load_tileset = async Texture.initFromFile(allocator, "assets/blocks.png");
     var load_font = async FontRenderer.initFromFile(allocator, "assets/PressStart2P_8.fnt");
+    var level: usize = 0;
 
     ctx = .{
         .grid = try Grid.init(allocator, vec(10, 20)),
@@ -66,13 +72,17 @@ pub fn onInit() !void {
         .last_time = 0,
         .bag = Piece.shuffled_bag(),
         .grab = 0,
-        .cleared_rows = [1]?usize{null} ** 4,
+        .cleared_rows = 0,
         .score = 0,
+        .level = level,
+        .level_at = level * 10,
 
         .tileset_tex = try await load_tileset,
         .flat = try FlatRenderer.init(allocator, seizer.getScreenSize().intToFloat(f32)),
         .font = try await load_font,
-        .score_text = try std.fmt.allocPrint(allocator, "{}", .{0}),
+        .score_text = try std.fmt.allocPrint(allocator, "Score: {}", .{0}),
+        .level_text = try std.fmt.allocPrint(allocator, "Level: {}", .{0}),
+        .lines_text = try std.fmt.allocPrint(allocator, "Lines: {}", .{0}),
     };
 
     grab_next_piece();
@@ -90,6 +100,8 @@ fn grab_next_piece() void {
 
 pub fn onDeinit() void {
     allocator.free(ctx.score_text);
+    allocator.free(ctx.level_text);
+    allocator.free(ctx.lines_text);
     ctx.grid.deinit();
     ctx.font.deinit();
     ctx.flat.deinit();
@@ -184,12 +196,14 @@ pub fn render(alpha: f64) !void {
     }
 
     ctx.font.drawText(&ctx.flat, ctx.score_text, vec(0, 0).intToFloat(f32), .{ .scale = 2, .textBaseline = .Top });
+    ctx.font.drawText(&ctx.flat, ctx.level_text, vec(0, 32).intToFloat(f32), .{ .scale = 2, .textBaseline = .Top });
+    ctx.font.drawText(&ctx.flat, ctx.lines_text, vec(0, 64).intToFloat(f32), .{ .scale = 2, .textBaseline = .Top });
 
     ctx.flat.flush();
 }
 
 pub fn update(current_time: f64, delta: f64) anyerror!void {
-    if (ctx.down_pressed or current_time - ctx.last_time > 1.0) {
+    if (ctx.down_pressed or current_time - ctx.last_time > get_drop_delta(ctx.level)) {
         var new_piece = ctx.piece;
         new_piece.move_down();
         if (new_piece.collides_with(&ctx.grid)) {
@@ -197,9 +211,20 @@ pub fn update(current_time: f64, delta: f64) anyerror!void {
             ctx.piece.integrate_with(&ctx.grid);
             grab_next_piece();
             var lines = try ctx.grid.clear_rows();
-            ctx.score += get_score(lines, 0);
+            ctx.cleared_rows += lines;
+            ctx.score += get_score(lines, ctx.level);
+
             allocator.free(ctx.score_text);
-            ctx.score_text = try std.fmt.allocPrint(allocator, "{}", .{ctx.score});
+            ctx.score_text = try std.fmt.allocPrint(allocator, "Score: {}", .{ctx.score});
+            allocator.free(ctx.level_text);
+            ctx.level_text = try std.fmt.allocPrint(allocator, "Level: {}", .{ctx.level});
+            allocator.free(ctx.lines_text);
+            ctx.lines_text = try std.fmt.allocPrint(allocator, "Lines: {}", .{ctx.cleared_rows});
+
+            if (ctx.cleared_rows > ctx.level_at) {
+                ctx.level += 1;
+                ctx.level_at += 10;
+            }
         } else {
             ctx.piece = new_piece;
         }
--- src/score.zig
+++ src/score.zig
@@ -8,3 +8,32 @@ const scoring_table: [5]usize = .{ 0, 40, 100, 300, 1200 };
 pub fn get_score(lines: usize, level: usize) usize {
     return scoring_table[lines] * (level + 1);
 }
+
+/// At 60 FPS, how many frames to wait before dropping the tetromino down another row at each level.
+/// Table pulled from https://harddrop.com/wiki/Tetris_(Game_Boy)
+// 0 	53
+// 1 	49
+// 2 	45
+// 3 	41
+// 4 	37
+// 5 	33
+// 6 	28
+// 7 	22
+// 8 	17
+// 9 	11
+// 10 	10
+// 11 	9
+// 12 	8
+// 13 	7
+// 14 	6
+// 15 	6
+// 16 	5
+// 17 	5
+// 18 	4
+// 19 	4
+// 20 	3
+const level_table: [10]usize = .{ 53, 49, 45, 41, 37, 33, 28, 22, 17, 11 };
+
+pub fn get_drop_delta(level: usize) f32 {
+    return 1.0 / (60.0 / @intToFloat(f32, level_table[level]));
+}
