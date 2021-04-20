6713d40353c7cfcb56b346647accb0973b456182
leroycep <leroycepearson@geemili.xyz>
2021-04-20 09:53:31 +0000

    Deploying to gh-pages from  @ b1d03b82c3d6dd7af0110b0dd3c378d822dae46f 🚀

---
+++ main.js
@@ -0,0 +1,17 @@
+import getPlatformEnv from "./seizer.js";
+
+const canvas_element = document.getElementById("game-canvas");
+var globalInstance;
+
+let env = {
+    ...getPlatformEnv(canvas_element, () => globalInstance),
+};
+
+fetch("blockstacker.wasm")
+    .then((response) => response.arrayBuffer())
+    .then((bytes) => WebAssembly.instantiate(bytes, { env }))
+    .then((results) => results.instance)
+    .then((instance) => {
+        globalInstance = instance;
+        instance.exports._start();
+    });
