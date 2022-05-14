dcb27c825f6b01a6574f8e94a5ddbd7693450f3c
Louis Pearson <louispearson@librem.one>
2022-05-14 16:02:34 -0600

    Update to zig master

--- tools/install_js.zig
+++ tools/install_js.zig
@@ -5,10 +5,11 @@ const chrono = @import("chrono");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    const args = try std.process.argsAlloc(&gpa.allocator);
-    defer std.process.argsFree(&gpa.allocator, args);
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
     const cwd = std.fs.cwd();
     const dir = try cwd.makeOpenPath(args[1], .{});
